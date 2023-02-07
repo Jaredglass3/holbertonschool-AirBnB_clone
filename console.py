9cdbfe6374c6156847d6f2da12220931c545c6ed
Jaredglass3 <Jaredglass352@gmail.com>
2023-02-07 16:42:53 +0000

    Corrected mistakes

--- console.py
+++ console.py
@@ -7,9 +7,9 @@ from models import storage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
-from models.amenity import Amenity
-from models.place import Place
 from models.city import City
+from models.place import Place
+from models.amenity import Amenity
 from models.review import Review
 
 
