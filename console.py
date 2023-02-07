54b508be199f2c41b8771901eb36dfafefaefa70
Jaredglass3 <Jaredglass352@gmail.com>
2023-02-07 16:14:33 +0000

    Updated console

--- console.py
+++ console.py
@@ -5,7 +5,7 @@ import re
 from shlex import split
 from models import storage
 from models.base_model import BaseModel
-
+from models.user import User
 
 def parse(arg):
     curly_braces = re.search(r"\{(.*?)\}", arg)
@@ -33,7 +33,8 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
     __classes = {
-        "BaseModel"
+        "BaseModel",
+        "User"
     }
 
     def emptyline(self):
